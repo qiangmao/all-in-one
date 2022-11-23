40c97e1eba5da699d5e534861b2ae9f689e751ad
maoqiang02 <maoqiang02@meituan.com>
2022-11-23 16:37:20 +0800

    添加一次性push到远端的脚本

---
+++ bin/push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+remotes=""
+function listRemotes() {
+    remotes=`git remote`
+}
+
+function push() {
+    listRemotes
+    for r in $remotes
+    do
+        echo "准备执行命令 git push $r $@"
+        git push $r "$@"
+    done
+}
+
+push "$@"
